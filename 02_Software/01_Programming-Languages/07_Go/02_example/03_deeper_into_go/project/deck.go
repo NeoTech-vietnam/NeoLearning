1f708e4a7d63b0c93d87cede1593d4b4517061fd
ThachVu <thach30012@gmail.com>
2026-05-10 13:07:31 +0700

    update: Upload learning marterials for Go languages

---
+++ 02_Software/01_Programming-Languages/07_Go/02_example/03_deeper_into_go/project/deck.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Create a new type of 'deck'
+// Which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// Video 25 - Deck to String
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
+}
+
+// Video 26 - Save to File
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666) // 0666 is the permission for the file that we can read and write
+}
