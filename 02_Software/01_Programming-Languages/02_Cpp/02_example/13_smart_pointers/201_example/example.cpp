eb0b0b40050982f9306f6eb31cc121103afc7cba
ThachVu <thach30012@gmail.com>
2026-06-09 04:47:36 +0700

    update: Add note and example for weak pointer

---
+++ 02_Software/01_Programming-Languages/02_Cpp/02_example/13_smart_pointers/201_example/example.cpp
@@ -0,0 +1,39 @@
+// Section 17
+// Weak pointer
+
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class B; // Forward declaration
+
+class A {
+    std::shared_ptr<B> b_ptr;
+public:
+    void set_B(std::shared_ptr<B> &b) {
+        b_ptr = b;
+    }
+    A() {cout << "A constructor " << endl; }
+    ~A() {cout << "A destructor " << endl; }
+};
+
+class B {
+    std::weak_ptr<A> a_ptr; // make weak to break the strong circular reference
+public:
+    void set_A(std::shared_ptr<A> &a) {
+        a_ptr = a;
+    }
+    B() {cout << "B constructor " << endl; }
+    ~B() {cout << "B destructor " << endl; }
+};
+
+int main (void){
+    shared_ptr<A> a = make_shared<A>();
+    shared_ptr<B> b = make_shared<B>();
+
+    a->set_B(b);
+    b->set_A(a);
+
+    return 0;
+}
