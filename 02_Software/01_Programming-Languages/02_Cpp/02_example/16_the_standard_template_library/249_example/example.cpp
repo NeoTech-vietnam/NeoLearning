c3b6944041798b2bdd6318d1e4aec3644e3e122c
ThachVu <thach30012@gmail.com>
2026-06-27 08:54:31 +0700

    update: Add example for sequence containers - vector

--- 02_Software/01_Programming-Languages/02_Cpp/02_example/16_the_standard_template_library/249_example/example.cpp
+++ 02_Software/01_Programming-Languages/02_Cpp/02_example/16_the_standard_template_library/249_example/example.cpp
@@ -74,11 +74,181 @@ void test2(){
     std::cout << "\nVec size: " << vec.size() << std::endl;
     std::cout << "\nVec max size: " << vec.max_size() << std::endl;
     std::cout << "\nVec capacity: " << vec.capacity() << std::endl;
-    
+}
+
+void test3() {
+    std::cout << "\nTest3 ===================================" << std::endl;
+    std::vector<int> vec {1,2,3,4,5};
+    display(vec);
+    vec[0] = 100;
+    vec.at(1) = 200;
+
+    display(vec);
+}
+
+void test4(){
+    std::cout << "\nTest4 ===================================" << std::endl;
+    std::vector<Person> stooges;
+
+    Person p1 {"Larry", 18};
+    display(stooges);
+
+    stooges.push_back(p1);
+    display(stooges);
+
+    stooges.push_back(Person{"Moe", 25});
+    display(stooges);
+
+    stooges.emplace_back("Curly", 30); // Use emplace_back!!
+    display(stooges);
+}
+
+void test5(){
+    std::cout << "\nTest5 ===================================" << std::endl;
+    std::vector<Person> stooges {
+        {"Larry", 18},
+        {"Moe", 25},
+        {"Curly", 30}
+    };
+
+    display(stooges);
+    std::cout << "\nFront: " << stooges.front() << std::endl;
+    std::cout << "Back: " << stooges.back() << std::endl;
+
+    stooges.pop_back();
+    display(stooges);
+}
+
+void test6(){
+    std::cout << "\nTest6 ===================================" << std::endl;
+    std::vector<int> vec{1,2,3,4,5};
+    display(vec);
+
+    vec.clear(); // remove all elements
+    display(vec);
+
+    vec = {1,2,3,4,5,6,7,8,9,10};
+    display(vec);
+    vec.erase(vec.begin(), vec.begin() + 2);
+    display(vec);
+
+    vec = {1,2,3,4,5,6,7,8,9,10};
+    // Erase all even numbers
+    auto it = vec.begin();
+    while(it != vec.end()) {
+        if(*it %2 == 0)
+            vec.erase(it);
+        else
+            it++; // Only increment if not erased!
+    }
+    display(vec);
+}
+
+void test7(){
+    std::cout << "\nTest7 ===================================" << std::endl;
+    std::vector<int> vec1 {1,2,3,4,5};
+    std::vector<int> vec2 {10,20,30,40,50};
+
+    display(vec1);
+    display(vec2);
+    std::cout << std::endl;
+
+    vec2.swap(vec1);
+    display(vec1);
+    display(vec2);
+}
+
+void test8(){
+    std::cout << "\nTest8 ===================================" << std::endl;
+    std::vector<int> vec1 {1,21,3,40,12};
+
+    display(vec1);
+    std::sort(vec1.begin(), vec1.end());
+    display(vec1);
+}
+
+void test9(){
+    // std::back_inseter contructs a back-insert iterator that inserts new eleemtns
+    // at the end of the container it applied to. It's a special output iterator
+    // It's awesome!!! and very efficient
+    // There is also a front_inserter we can use with deques and lists
+    // Copy one list to another using an iterator and back_inseter
+
+    std::cout << "\nTest9 ===================================" << std::endl;
+
+    std::vector<int> vec1 {1,2,3,4,5};
+    std::vector<int> vec2 {10,20};
+
+    display(vec1);
+    display(vec2);
+    std::cout << std::endl;
+
+    std::copy(vec1.begin(), vec1.end(), std::back_inserter(vec2));
+    display(vec1);
+    display(vec2);
+    std::cout << std::endl;
+
+    // Copy_if the eleemnt is even
+
+    vec1 = {1,2,3,4,5,6,7,8,9,10};
+    vec2 = {10,20};
+
+    display(vec1);
+    display(vec2);
+    std::cout << std::endl;
+
+    std::copy_if(vec1.begin(), vec1.end(), std::back_inserter(vec2), [](int x) {return x%2 == 0;});
+    display(vec1);
+    display(vec2);
+
+}
+
+void test10() {
+    std::cout << "\nTest10 ===================================" << std::endl;
+    // Transform over 2 ranges
+
+    std::vector<int> vec1 {1,2,3,4,5};
+    std::vector<int> vec2 {10,20,30,40,50};
+    std::vector<int> vec3;
+
+    //1*10, 2*20, 3*30, 4*40, 5*50 and store the results in vec3
+    std::transform(vec1.begin(), vec1.end(), vec2.begin(), std::back_inserter(vec3), [](int x, int y) {return x * y;});
+
+    display(vec3);
+}
+
+void test11(){
+    std::cout << "\nTest11 ===================================" << std::endl;
+
+    std::vector<int> vec1 {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> vec2 {100,200,300,400};
+
+    display(vec1);
+    display(vec2);
+    std::cout << std::endl;
+
+    auto it = std::find(vec1.begin(), vec1.end(), 5);
+    if(it != vec1.end()){
+        std::cout << "inserting..." << std::endl;
+        vec1.insert(it, vec2.begin(), vec2.end());
+    }
+    else{
+        std::cout << "Sorry, 5 not found" << std::endl;
+    }
+    display(vec1);
 }
 
 int main(void){
     // test1();
-    test2();
+    // test2();
+    // test3();
+    // test4();
+    // test5();
+    // test6();
+    test7();
+    test8();
+    test9();
+    test10();
+    test11();
     return 0;
 }
