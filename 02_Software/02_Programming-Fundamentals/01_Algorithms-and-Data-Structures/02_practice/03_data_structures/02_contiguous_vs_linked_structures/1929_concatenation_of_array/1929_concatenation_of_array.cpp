1c4e7e7b530a91b557f20b89533a308838b88502
ThachVu <thach30012@gmail.com>
2026-08-22 11:10:43 +0700

    update: Add more descriptions for solution 1929

--- 02_Software/02_Programming-Fundamentals/01_Algorithms-and-Data-Structures/02_practice/03_data_structures/02_contiguous_vs_linked_structures/1929_concatenation_of_array/1929_concatenation_of_array.cpp
+++ 02_Software/02_Programming-Fundamentals/01_Algorithms-and-Data-Structures/02_practice/03_data_structures/02_contiguous_vs_linked_structures/1929_concatenation_of_array/1929_concatenation_of_array.cpp
@@ -37,13 +37,20 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = static_cast<int>(nums.size());
-        vector<int> ans {2*n};
+        vector<int> ans (2*n); // {} is used for assign inital value
         for(int i = 0; i < n; i++){
             ans[i] = nums[i];
             ans[i + n] = nums[i];
         }
         return ans;
     }
+    /* STL method
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans = nums;
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        return ans;
+    }
+    */
 };
 
 static bool areEqual(const vector<int>& a, const vector<int>& b) {
